8567ceae85a8e605ce9576712c3ea4e0dfeb2a1a
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2023-09-18 02:50:37 +0530

    Added Static File Directory & Changed UI

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const port = 3000;
 const API_URL = "";
 
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("",(req,res)=>{
